103b601d1f409ad893c67918126cabb0c921db0e
SBTHDR <subrata.halder13d@gmail.com>
2022-01-01 23:37:45 +0600

    Setup Template Engine

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-    res.send('Home Route')
+    res.render('home')
 })
+
+// Setup Template Engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, './resources/views'))
+app.set('view engine', 'ejs')
 
 app.listen(PORT, () => { 
     console.log(`Server is running on port ${PORT}`)
