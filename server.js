440cb63b405e70a936ecf082673f176b6cfed114
SBTHDR <subrata.halder13d@gmail.com>
2022-01-01 23:27:04 +0600

    Init express routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+    res.send('Home Route')
+})
+
 app.listen(PORT, () => { 
     console.log(`Server is running on port ${PORT}`)
 })
